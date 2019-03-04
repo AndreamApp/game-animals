b007be00ba0d7577a6d2483c16f53957142cc917
Peng QiBiao <andreamapp@sina.com>
2019-03-04 15:43:36 +0800

    [Add] SettingsActivity layout

--- app/src/main/java/app/andream/game/SettingsActivity.java
+++ app/src/main/java/app/andream/game/SettingsActivity.java
@@ -2,6 +2,7 @@ package app.andream.game;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 /**
  * Created by Andream on 2019/3/4.
@@ -10,10 +11,14 @@ import android.support.v7.app.AppCompatActivity;
  */
 public class SettingsActivity extends AppCompatActivity {
 
+    private EditText roundEditor, durationEditor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        roundEditor = findViewById(R.id.et_round);
+        durationEditor = findViewById(R.id.et_duration);
     }
 
 }
